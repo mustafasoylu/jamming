9f5b7081648bc1f57bed1fa1316b45c274657b42
Mustafa SOYLU <soylumu@hotmail.com>
2021-08-31 14:53:02 +0900

    step 30-31-32

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,11 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchResults: [] }
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +18,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults} />
             <Playlist />
           </div>
         </div>
