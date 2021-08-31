161c7f5c695acac364766ddc7c251b36fc3273ca
Mustafa SOYLU <soylumu@hotmail.com>
2021-08-31 15:08:45 +0900

    step 45-46-47

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,10 +2,19 @@ import { React, Component } from 'react';
 import "./Track.css";
 
 export class Track extends Component {
+    constructor(props) {
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+    }
+
     renderAction() {
         let buttonString = "+";
         if (this.props.isRemoval) { buttonString = "-"; }
         return buttonString;
+    }
+
+    addTrack() {
+        this.props.onAdd(this.props.track);
     }
 
     render() {
@@ -15,7 +24,7 @@ export class Track extends Component {
                     <h3>{this.props.track.name}</h3>
                     <p>{this.props.track.artist}| {this.props.track.album}</p>
                 </div>
-                <button className="Track-action">{this.renderAction()}</button>
+                <button className="Track-action" onClick={this.addTrack} >{this.renderAction()}</button>
             </div>
         );
     }
