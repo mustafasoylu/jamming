14d66d9be69105815f21c979cec91ecd04949727
Mustafa SOYLU <soylumu@hotmail.com>
2021-08-31 14:31:32 +0900

    step 14-15

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import "./App.css";
 
+import SearchBar from '../SearchBar/SearchBar.js';
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Ja<span class="highlight">mmm</span>ing</h1>
         <div class="App">
+          <SearchBar />
           <div class="App-playlist">
           </div>
         </div>
