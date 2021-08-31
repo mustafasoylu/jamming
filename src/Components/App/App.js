b9ccd0aaf4074774fcf4b47b5a63c4d07d58b349
Mustafa SOYLU <soylumu@hotmail.com>
2021-08-31 14:38:21 +0900

    step 21

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,6 +11,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
           </div>
         </div>
       </div>
